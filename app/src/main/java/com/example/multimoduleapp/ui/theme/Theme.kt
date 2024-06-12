7ebd5a0b91d3e3c22544f989738c873375a438eb
Viraj76 <virajg.7680@gmail.com>
2024-06-13 03:28:52 +0530

    initial setup

--- app/src/main/java/com/example/multimoduleapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/multimoduleapp/ui/theme/Theme.kt
@@ -1,58 +1,54 @@
 package com.example.multimoduleapp.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val DarkColorPalette = darkColors(
+    primary = Blue300,
+    primaryVariant = Blue700,
+    onPrimary = Color.White,
+    secondary = Color.Black,
+    secondaryVariant = Teal300,
+    onSecondary = Color.White,
+    error = RedErrorLight,
+    onError = RedErrorDark,
+    background = Color.Black,
+    onBackground = Color.White,
+    surface = Black1,
+    onSurface = Color.White,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val LightColorPalette = lightColors(
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Black1,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    onSecondary = Color.Black,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Grey1,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Black1,
 )
 
 @Composable
-fun MultiModuleAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+fun MultiModuleAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if(darkTheme){
+        DarkColorPalette
+    } else{
+        LightColorPalette
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+        colors = colors,
+        typography = QuickSandTypography,
+        shapes = AppShapes,
         content = content
     )
 }
